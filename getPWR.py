100009f7b264bb8298abee8035beb036b9bf2331
avsteckel <avsteckel@gmail.com>
2015-07-21 09:44:44 -0400

    Added the plots!

--- getPWR.py
+++ getPWR.py
@@ -1,1 +1,13 @@
-# this is gonna get power
+# this is gonna get the mass estimate assuming the battery type
+# dry mass of the spacecraft is 30 kg without batteries, including pld
+
+import math
+
+dryMass = 10
+mass_per_unit = 2.2 # kg
+cap = 12 # Ahr
+V = 12 # V
+A = 400 # Amps
+time = 10 # sec
+
+pwr = V*A
--- getVel.py
+++ getVel.py
@@ -1,21 +1,27 @@
+# Set up python and import packages for GUI and matlab calcs
 import math
 from math import pi, sin, cos
-import matplotlib as mpl
-from numpy import floor
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+
+import time
+from numpy import floor, arange, sin, pi
 import numpy as np
-import matplotlib.pyplot as plt
+
 import Tkinter
 import tkMessageBox
 from Tkinter import *
 from decimal import *
 
+# get the velocity for the given r, mdot and RPM
 def getMdot(r,mdot,RPM):
     res=int(100)
     phi1 = np.deg2rad(45)
     vp1_theta=[]
     vp1_z=[]
     tvec=[]
-    P=[]  # array to
     thrust=float(0)
     # Assume an equal distribution of particles along the radius
     rad = np.linspace(0,float(r)/100,num = res)
@@ -39,8 +45,26 @@ def getPWR(mdot,zvel):
     #print("Power: ",power)
     return pwr
 
+def getBatt(p):
+    # this is gonna get the mass estimate assuming the battery type
+    #dry mass of the spacecraft is 30 kg without batteries, including pld
+    dryMass = 10
+    mass_per_unit = 2.2 # kg
+    cap = 12 # Ahr
+    V = 12 # V
+    A = 400 # Amps
+    time = 10 # sec
 
-root = Tkinter.Tk()
+    pwr = V*A/1000 #in kW
+    numBatt=np.floor(float(p)/pwr)+1
+    return numBatt
+
+#def getPerformance():
+    # normalize the parameters to a
+
+
+root = Tk()
+root.title("Regolith Thruster Shenanagins")
 var1 = StringVar()
 var2 = StringVar()
 var3 = StringVar()
@@ -74,10 +98,31 @@ Tkinter.Label(root, text="cm",borderwidth=1).grid(row=5,column=4)
 Tkinter.Label(root, text="Power",borderwidth=1).grid(row=6,column=0)
 Tkinter.Label(root, text="kW",borderwidth=1).grid(row=6,column=2)
 
+# Make a plot
+f = Figure()
+a = f.add_subplot(111)
+x = [0]
+perf = [0] # some function
+
+a.plot(x,perf)
+a.set_xlabel("Mass")
+a.set_title("Performance of the design")
+a.set_ylabel("Total Performance Score (0-10)")
+
+
+# Draw the plot!
+root2 = Tk()#.Tk()
+root2.title("Plot")
+#canvas = Tkinter.Canvas(root, bg="white").grid(row=8,column=1)
+canvas = FigureCanvasTkAgg(f, master=root2)
+canvas.get_tk_widget().grid(row=0,column=0)
+#.pack()
+
 # Update GUI
-def CallBack():
+def Compute():
     [thrust,zvel,rmax]=getMdot(float(r.get()),float(mdot.get()),float(RPM.get()))
     pwr=getPWR(float(mdot.get()),zvel)
+    numBatt = getBatt(pwr)
     var1.set(thrust)
     var2.set(zvel)
     var3.set(rmax)
@@ -86,8 +131,18 @@ def CallBack():
     Tkinter.Label = Message( root, textvariable=var2,width=50).grid(row=5,column=1)
     Tkinter.Label = Message( root, textvariable=var3,width=50).grid(row=5,column=3)
     Tkinter.Label = Message( root, textvariable=var4,width=50).grid(row=6,column=1)
-    #tkMessageBox.showinfo( "Hello Python", "Hello World")
+    x.append(numBatt)
+    perf.append(thrust)
 
-B = Button(root, text ="Compute", command = CallBack).grid(row=7,column=1)
+    f = Figure(figsize=(5,4), dpi=100)
+    a = f.add_subplot(111)
+    a.plot(x,perf)
+    canvas = FigureCanvasTkAgg(f, master=root2)
+    canvas.get_tk_widget().grid(row=0,column=1)
+    tkMessageBox.showinfo( "number of batteries", x)
+    tkMessageBox.showinfo( "thrust", perf)
+
+
+B = Button(root, text ="Compute", command = Compute).grid(row=7,column=1)
 
 root.mainloop()
